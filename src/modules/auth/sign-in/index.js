45dbaf73c8e2410557a85f0317258eb0e1085233
muslimragimov <muslim.ragimov@yahoo.com>
2024-01-22 10:33:08 +0400

    Minor changes

---
+++ src/modules/auth/sign-in/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function SignInModule() {
+    return (
+        <div className='auth-wrapper'>
+            Sign In
+        </div>
+    )
+}
---
+++ src/pages/sign-in/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Head from 'next/head';
+import dynamic from "next/dynamic";
+
+
+const SignInModule = dynamic(
+    () => import('@/modules/auth/sign-in'),
+    { ssr: false }
+)
+
+
+export default function ResultsPage() {
+    return (
+        <>
+            <Head>
+                <title>Legalens | Sign In Page</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <SignInModule />
+        </>
+    );
+}
